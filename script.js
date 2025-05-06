eecf84b5551f4dafff555b9167662544ac09eee5
mounibwebdev12 <mounibkaouanidz@gmail.com>
2025-05-06 21:44:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toolsButton").onclick = function() {
+  const panel = document.getElementById("toolsPanel");
+  panel.style.display = panel.style.display === "none" ? "block" : "none";
+};
